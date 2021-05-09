5cd99a749f97b56598e46e79534d31241ac6412e
wzhe@outlook.com <wzhe@outlook.com>
2021-05-09 09:22:54 +0800

    feature test2

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 >>>>>>> feature test1
         </p>
         <p>feature test1</p>
+        <p>test2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
