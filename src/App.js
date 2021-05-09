092e51f3abff38837f85b69a1159eaece5985c21
wzhe@outlook.com <wzhe@outlook.com>
2021-05-09 09:17:28 +0800

    feature test1

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+<<<<<<< HEAD
           Edit <code>src/App.js</code> and save to reload..
+=======
+          Edit <code>src/App.js</code> and save to reload. 
+>>>>>>> feature test1
         </p>
+        <p>feature test1</p>
         <a
           className="App-link"
           href="https://reactjs.org"
